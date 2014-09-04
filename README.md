7f4fb6170e90cc74d107f63080a35fcef086c961
lhfcws <lhfcws@163.com>
2014-09-04 21:38:19 +0800

    rdir

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Recursively look up python module and documents.
 Refer to `test.py` for example.
 
 
-> TODO:  
-> 1. Add docs  
-> 2. pretty print  
+> TODO:
+> 1. Add setup.py
+> 2. Add docs
+> 3. pretty print
+
--- rdir.py
+++ rdir.py
@@ -4,7 +4,8 @@ __author__ = 'lhfcws'
 
 import copy
 
-class __TempModule(object):
+
+class _TempModule(object):
     def __init__(self):
         self.modules = {}
 
@@ -13,19 +14,19 @@ class __TempModule(object):
         self.modules[module_name] = mod
 
 
-__temp_module = __TempModule()
+_temp_module = _TempModule()
 
 
-def __line_prefix(num):
+def _line_prefix(num):
     return "-" * num
 
 
-def __blank_prefix(num):
+def _blank_prefix(num):
     return " " * num
 
 
-def __get_doc(name, prefix):
-    doc = eval(name + ".__doc__", __temp_module.modules)
+def _get_doc(name, prefix):
+    doc = eval(name + ".__doc__", _temp_module.modules)
     if doc is None:
         doc = ""
     else:
@@ -33,41 +34,41 @@ def __get_doc(name, prefix):
     return doc
 
 
-def __dir(name):
+def _dir(name):
     return "dir(" + name + ")"
 
 
-def __get_children(name):
-    ls = eval(__dir(name), __temp_module.modules)
+def _get_children(name):
+    ls = eval(_dir(name), _temp_module.modules)
     res = filter(lambda child: not child.startswith("_"), ls)
     return res
 
 
-def __get_full_name(paths):
+def _get_full_name(paths):
     return ".".join(paths)
 
 
-def __get_type(name):
-    return " (" + str(eval("type(" + name + ")", __temp_module.modules)) + ")"
+def _get_type(name):
+    return " (" + str(eval("type(" + name + ")", _temp_module.modules)) + ")"
 
 
-def __recursive_dir_with_doc(deep, mod_name, parents, limit_deep):
-    line_prefix = __line_prefix(deep) + " "
-    blank_prefix = __blank_prefix(deep) + " "
+def _recursive_dir_with_doc(deep, mod_name, parents, limit_deep):
+    line_prefix = _line_prefix(deep) + " "
+    blank_prefix = _blank_prefix(deep) + " "
     p = copy.deepcopy(parents)
     p.append(mod_name)
 
-    full_name = __get_full_name(p)
+    full_name = _get_full_name(p)
 
-    output = line_prefix + mod_name + __get_type(full_name) + " :\n" + blank_prefix + __get_doc(full_name, blank_prefix)
+    output = line_prefix + mod_name + _get_type(full_name) + " :\n" + blank_prefix + _get_doc(full_name, blank_prefix)
     print output
 
-    if deep == limit_deep:
+    if limit_deep != -1 and deep == limit_deep:
         return
 
-    children = __get_children(full_name)
+    children = _get_children(full_name)
     for child in children:
-        __recursive_dir_with_doc(deep + 1, child, p, limit_deep)
+        _recursive_dir_with_doc(deep + 1, child, p, limit_deep)
 
 
 def rdir(module, limit_deep=3):
@@ -75,10 +76,10 @@ def rdir(module, limit_deep=3):
     assert type(module) == type(str())
     print "Parsing module: " + module
 
-    __temp_module.import_module(module)
+    _temp_module.import_module(module)
     deep = 0
 
     if type(module) == type(str()):
-        __recursive_dir_with_doc(deep, module, [], limit_deep)
+        _recursive_dir_with_doc(deep, module, [], limit_deep)
     else:
         raise Exception("Invalid param `module`, it should be a string or a module.")
