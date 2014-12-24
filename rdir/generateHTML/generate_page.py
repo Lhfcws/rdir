5986df3e021d123d133e5c1298278a5e2abba51e
laiy <ly.franky@gmail.com>
2014-12-24 21:04:12 +0800

    I am even stupider than I thought

--- rdir/generateHTML/generate_page.py
+++ rdir/generateHTML/generate_page.py
@@ -14,20 +14,28 @@ from pyquery import PyQuery
 class HTMLGenerator:
 
     def __init__(self):
-        pass
-
-    def generate_tree_structure_HTML(self, rdir_node):
         with open(os.path.join(os.path.dirname(__file__), 'template', 'tree_template.html')) as f:
             self.template = PyQuery(f.read().decode('utf-8'))
+        with open(os.path.join(os.path.dirname(__file__), 'template', 'tree_node_template.html')) as f:
+            self.node_template = PyQuery(f.read().decode('utf-8'))
+
+    def generate_tree_structure_HTML(self, rdir_node):
         self.template('title').html(rdir_node.name)
         for key in rdir_node.list_children():
-            self._add_node(rdir_node.get_children(key), 0)
+            self._add_node_recursively(rdir_node.get_children(key), 0)
+        print(self.template.html())
 
-    def _add_node(self, rdir_node, depth):
-        self._add_node_to_HTML(rdir_node.name, rdir_node.doc, rdir_node.type, depth)
+    def _add_node_recursively(self, rdir_node, depth):
+        self._add_node_to_HTML(rdir_node.name, rdir_node.doc, rdir_node.type[9 : -3], depth)
         for key in rdir_node.list_children():
-            self._add_node(rdir_node.get_children(key), depth + 1)
+            self._add_node_recursively(rdir_node.get_children(key), depth + 1)
 
     def _add_node_to_HTML(self, fullname, doc, obj_type, depth):
-        pass
+        node = self.node_template
+        node('.tree_node').css('margin-left', str(depth * 50) + 'px')
+        node('.interval').css('margin-left', str(depth * 50) + 'px')
+        node('.node_fullname').html(fullname)
+        node('.node_type').html(obj_type)
+        node('.node_doc').html(doc)
+        self.template('body').append(node.html())
 
