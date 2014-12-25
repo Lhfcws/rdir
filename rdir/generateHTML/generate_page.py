0837654d9acf35e632636b5a7867af6b13ed83da
laiy <ly.franky@gmail.com>
2014-12-25 09:51:43 +0800

    project lead is allergic to changes...

--- rdir/generateHTML/generate_page.py
+++ rdir/generateHTML/generate_page.py
@@ -9,7 +9,11 @@
 __author__ = 'laiy'
 
 import os
+import sys
 from pyquery import PyQuery
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 class HTMLGenerator:
     """HTML Generator
@@ -18,31 +22,41 @@ class HTMLGenerator:
     def __init__(self):
         with open(os.path.join(os.path.dirname(__file__), 'template', 'tree_template.html')) as f:
             self.template = PyQuery(f.read().decode('utf-8'))
+
+    def generate_tree_structure_HTML(self, root_node):
+        """Generate a html file with tree structure.
+        :param root_node: RDirNode root of the module
+        """
         with open(os.path.join(os.path.dirname(__file__), 'template', 'tree_node_template.html')) as f:
             self.node_template = PyQuery(f.read().decode('utf-8'))
         print(self.node_template.html())
-
-    def generate_tree_structure_HTML(self, rdir_node):
-        """Generate a html file with tree structure
-        """
-        self.template('title').html(rdir_node.name)
-        for key in rdir_node.list_children():
-            self._add_node_recursively(rdir_node.get_children(key), 0)
-        os.system(r'touch %s' % os.path.join(os.path.dirname(__file__), 'generatedHTML', rdir_node.name + '.html'))
-        with open(os.path.join(os.path.dirname(__file__), 'generatedHTML', rdir_node.name + '.html'), 'w') as f:
+        self.template('title').html(root_node.name)
+        for key in root_node.list_children():
+            self._add_node_recursively(root_node.get_children(key), 0)
+        with open(os.path.join(os.path.dirname(__file__), 'generatedHTML', root_node.name + '.html'), 'w') as f:
             f.write(self.template.html())
 
     def _add_node_recursively(self, rdir_node, depth):
+        """Add node recursively to the views.
+        :param rdir_node: RDirNode node to be added
+        :param depth: int current recursive depth
+        """
         self._add_node_to_HTML(rdir_node.name, rdir_node.doc, rdir_node.type[9 : -3], depth)
         for key in rdir_node.list_children():
             self._add_node_recursively(rdir_node.get_children(key), depth + 1)
 
     def _add_node_to_HTML(self, fullname, doc, obj_type, depth):
+        """Add a DOM node to HTML template.
+        :param fullname: str object full name like "urllib2.opener"
+        :param doc: str object document
+        :param obj_type: str object type
+        :param depth: int current recursive depth
+        """
         node = self.node_template
         node('.tree_node').css('margin-left', str(depth * 50) + 'px')
         node('.interval').css('margin-left', str(depth * 50) + 'px')
         node('.node_fullname').html(fullname)
         node('.node_type').html(obj_type)
-        node('.node_doc').html(doc)
+        node('.node_doc').html(doc.replace(' ', '&nbsp;').replace('\n', '<br/>'))
         self.template('body').append(node.html())
 
