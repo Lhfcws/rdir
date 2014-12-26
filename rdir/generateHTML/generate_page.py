69428ecde804e5a4418c03d08726443875a95c3e
lhfcws <lhfcws@163.com>
2014-12-26 12:06:52 +0800

    for ly

--- rdir/generateHTML/generate_page.py
+++ rdir/generateHTML/generate_page.py
@@ -9,7 +9,8 @@ from pyquery import PyQuery
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-class HTMLGenerator:
+
+class HTMLGenerator(object):
     """HTML Generator
     """
 
@@ -19,14 +20,18 @@ class HTMLGenerator:
         with open(os.path.join(os.path.dirname(__file__), 'template', 'tree_node_template.html')) as f:
             self.node_template = PyQuery(f.read(), parser='html')
 
-    def generate_tree_structure_HTML(self, root_node):
+    def generate_tree_structure_HTML(self, root_node, output):
         """Generate a html file with tree structure.
         :param root_node: RDirNode root of the module
         """
+
+        self.template('#header_text').html(root_node.name)
         self.template('title').html(root_node.name)
         for key in root_node.list_children():
             self._add_node_recursively(root_node.get_children(key), 0)
-        with open(os.path.join(os.path.dirname(__file__), 'generatedHTML', root_node.name + '.html'), 'w') as f:
+        # with open(os.path.join(os.path.dirname(__file__), 'generatedHTML', root_node.name + '.html'), 'w') as f:
+        # use user defined output path
+        with open(output, 'w') as f:
             f.write(self.template.html())
 
     def _add_node_recursively(self, rdir_node, depth):
